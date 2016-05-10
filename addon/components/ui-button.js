877d83d978271923c8bcf08d5a02beb54f88a3d5
John Otander <johnotander@gmail.com>
2016-05-09 21:04:11 -0600

    Add configurable font size variables
    This creates type scale classes for font-size
    variables and implements a potential use
    in the ui-button component.

--- addon/components/ui-button.js
+++ addon/components/ui-button.js
@@ -4,11 +4,24 @@ import layout from 'untitled-ui/templates/components/ui-button';
 export default Ember.Component.extend({
   layout,
 
-  tagName: '',  
+  tagName: '',
   kind: 'default',
   size: 'medium',
   disabled: false,
   loading: false,
   
-  isDisabled: Ember.computed.or('disabled', 'loading')
+  isDisabled: Ember.computed.or('disabled', 'loading'),
+
+  classes: Ember.computed('size', 'kind', function() {
+    let classes = [
+      'ui-button', `ui-button--${this.get('kind')}`,
+      `ui-fontSize--${this.get('size')}`
+    ];
+
+    if (this.get('isDisabled')) {
+      classes.push('ui-button--disabled');
+    }
+
+    return classes.join(' ');
+  })
 });
