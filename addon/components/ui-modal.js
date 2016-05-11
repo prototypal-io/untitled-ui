d6808937b0eac258b6fb0a977992109d76cc8d74
Jaco Joubert <joubert.jaco@gmail.com>
2016-05-11 16:52:38 -0400

    add basic styling for modal, allow ESC to close modal

--- addon/components/ui-modal.js
+++ addon/components/ui-modal.js
@@ -3,7 +3,23 @@ import layout from '../templates/components/ui-modal';
 
 export default Ember.Component.extend({
   layout,
-  
+
+  attributeBindings: ['tabindex'],
+
+  // Allows modal to reveive focus so we can catch key presses
+  tabindex: 0,
+
+  didInsertElement() {
+    this.$().focus();
+  },
+
+  keyUp(event) {
+    // ESC
+    if (event.which === 27) {
+      this.sendAction('onclose');
+    }
+  },
+
   actions: {
     dismiss() {
       this.sendAction('onclose');
