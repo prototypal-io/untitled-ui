2ffe6f0abd2e0b3afef3b4dfc10fae2143895cbc
Jaco Joubert <joubert.jaco@gmail.com>
2016-05-17 19:23:30 -0400

    restore modal titlebar left/right components

---
+++ addon/components/ui-panel-titlebar--modal-action.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+import layout from '../templates/components/ui-panel-titlebar--modal-action';
+
+export default Ember.Component.extend({
+  layout,
+
+  tagName: '',
+  location: 'left'
+});
