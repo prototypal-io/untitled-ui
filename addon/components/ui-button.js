909509e258ac1a1683c46497e1a8dde7cb42f823
Jaco Joubert <joubert.jaco@gmail.com>
2016-05-18 14:32:55 -0400

    ui-button cleanup. - move button tag into kind components - clean up class names - use hashes for actions and statuses

--- addon/components/ui-button.js
+++ addon/components/ui-button.js
@@ -4,26 +4,39 @@ import layout from 'untitled-ui/templates/components/ui-button';
 export default Ember.Component.extend({
   layout,
 
-  tagName: '',
-  size: 'medium',
+  tagName: "",
+  kind: "default",
+  size: "medium",
+
   disabled: false,
   loading: false,
   active: false,
   focus: false,
-  kind: "default",
 
   frame: Ember.computed('kind', function() {
     return `ui-button--${this.get('kind')}`;
   }),
 
+  classes: Ember.computed('size', function() {
+    return {
+      parent: 'ui-button',
+      size: `ui-fontSize--${this.get('size')}`
+    }
+  }),
+
   isDisabled: Ember.computed.or('disabled', 'loading'),
 
-  sizeClass: Ember.computed('size', function() {
-    return `ui-fontSize--${this.get('size')}`;
+  states: Ember.computed('isDisabled', 'loading', 'active', 'focus', function() {
+    return {
+      disabled: this.get('isDisabled'),
+      loading: this.get('loading'),
+      active: this.get('active'),
+      focus: this.get('focus')
+    }
   }),
 
   $(sel) {
-    let el = this._renderNode.firstNode;
+    let el = this._renderNode.firstNode.nextSibling;
     return sel ? $(sel, el) : $(el);
   },
 
@@ -31,9 +44,11 @@ export default Ember.Component.extend({
     this.$().on('webkitTransitionEnd transitionend msTransitionEnd oTransitionEnd', () => {
       this.toggleProperty('active');
     });
+
     this.$().on('focusin', () => {
       this.set('focus', true);
     });
+
     this.$().on('focusout', () => {
       this.set('focus', false);
     });
