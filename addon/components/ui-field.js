2119034274873373df7e706d693eba0cf92beb81
Jaco Joubert <joubert.jaco@gmail.com>
2016-05-19 18:45:29 -0400

    convert ui-field to new theming structure. #11

--- addon/components/ui-field.js
+++ addon/components/ui-field.js
@@ -5,15 +5,33 @@ export default Ember.Component.extend({
   layout,
   tagName: '',
 
-  label: null,
   type: 'ui-input',
+
+  kind: 'default',
   size: 'medium',
-  
+
   generatedInputId: Ember.computed(function() {
     return `${Ember.guidFor(this)}__input`;
   }),
 
   generatedLabelId: Ember.computed(function() {
     return `${Ember.guidFor(this)}__label`;
+  }),
+
+  frame: Ember.computed('kind', function() {
+    return `ui-field--${this.get('kind')}`;
+  }),
+
+  classes: Ember.computed('size', function() {
+    return {
+      parent: 'ui-field',
+      size: `ui-font-size--${this.get('size')}`
+    }
+  }),
+
+  states: Ember.computed('disabled', function() {
+    return {
+      disabled: this.get('disabled')
+    }
   }),
 });
