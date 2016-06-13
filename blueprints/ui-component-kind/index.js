4636c9d3e1afd02fad4583af2a2f05f22bc639de
Eric Kelly <heroiceric@gmail.com>
2016-06-13 19:06:56 -0400

    Add ui-component-kind blueprint

---
+++ blueprints/ui-component-kind/index.js
@@ -0,0 +1,24 @@
+/*jshint node:true*/
+module.exports = {
+  description: 'Generate a UI Component Kind',
+
+  availableOptions: [
+    {
+      name: 'path',
+      type: String,
+      default: 'components'
+    }
+  ],
+
+  fileMapTokens: function() {
+    return this.lookupBlueprint('component').fileMapTokens();
+  },
+
+  normalizeEntityName: function(entityName) {
+    return this.lookupBlueprint('component').normalizeEntityName(entityName);
+  },
+
+  locals: function(options) {
+    return this.lookupBlueprint('component').locals(options);
+  }
+};
