e69115c617e32a70b350d9a622026e73ccab6ecd
Jaco Joubert <joubert.jaco@gmail.com>
2016-06-16 13:04:07 -0400

    update ui-select to new format #12 - add support for kind - create default style - add disabled support

--- addon/components/ui-select.js
+++ addon/components/ui-select.js
@@ -3,5 +3,27 @@ import layout from '../templates/components/ui-select';
 
 export default Ember.Component.extend({
   layout,
-  tagName: ''
+  tagName: '',
+
+  kind: 'default',
+  disabled: false,
+  error: false,
+
+  frame: Ember.computed('kind', function() {
+    return `ui-select--${this.get('kind')}`;
+  }),
+
+  classes: Ember.computed('class', 'size', function() {
+    return {
+      class: this.get('class'),
+      size: `ui-font-size--${this.get('size')}`
+    };
+  }),
+
+  states: Ember.computed('disabled', 'error', function() {
+    return {
+      disabled: this.get('disabled'),
+      error: this.get('error')
+    }
+  })
 });
