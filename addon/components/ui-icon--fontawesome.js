ab00f268c574222f28d558cbc887efbf7b7acb89
Jaco Joubert <joubert.jaco@gmail.com>
2016-06-19 12:01:55 -0400

    Switch ui-icon--fontawesome to using Font Awesome's size classes. #38

--- addon/components/ui-icon--fontawesome.js
+++ addon/components/ui-icon--fontawesome.js
@@ -1,9 +1,20 @@
 import Ember from 'ember';
 import layout from 'untitled-ui/templates/components/ui-icon--fontawesome';
 
+const TYPE_SCALE = ['x-small', 'small', 'medium', 'large', 'x-large'];
+
 export default Ember.Component.extend({
   layout,
   tagName: '',
 
-  size: 'lg'
+  fontAwesomeSize: Ember.computed('size', function() {
+    const size = this.get('size');
+
+    if (TYPE_SCALE.indexOf(size) !== -1) {
+      return 'fa-lg';
+
+    } else {
+      return size;
+    }
+  })
 });
