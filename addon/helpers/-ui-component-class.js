390b2f54efe86ae962be1b1f37dfa811ccf7ce7f
Jaco Joubert <joubert.jaco@gmail.com>
2016-06-19 11:51:23 -0400

    Add rewriting exception for Font Awesome class names. #38

--- addon/helpers/-ui-component-class.js
+++ addon/helpers/-ui-component-class.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 const FONT_SIZE_PATTERN = /font-size/;
+const FONTAWESOME_PATTERN = /^fa-/;
 
 export default Ember.Helper.helper(function ([prefix, ...classNames]) {
   let normalizedClassNames = classNames.reduce(function(names, name) {
@@ -21,6 +22,8 @@ export default Ember.Helper.helper(function ([prefix, ...classNames]) {
         var baseClass = prefix.replace(/(.*)--$/, '$1');
         return string += `${baseClass} `;
       case (FONT_SIZE_PATTERN.test(name)):
+        return string += `${name} `;
+      case (name === 'fa' || FONTAWESOME_PATTERN.test(name)):
         return string += `${name} `;
       default:
         return string += `${prefix}${name} `;
