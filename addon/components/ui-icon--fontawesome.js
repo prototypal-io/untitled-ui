7230f38f02e3eda6c7718b78bc2aabd2a3989882
Jaco Joubert <joubert.jaco@gmail.com>
2016-06-19 12:03:09 -0400

    switch ui-icon--fontawesome to using UIKindComponent #38

--- addon/components/ui-icon--fontawesome.js
+++ addon/components/ui-icon--fontawesome.js
@@ -1,11 +1,11 @@
 import Ember from 'ember';
+import UIKindComponent from './ui-kind';
 import layout from 'untitled-ui/templates/components/ui-icon--fontawesome';
 
 const TYPE_SCALE = ['x-small', 'small', 'medium', 'large', 'x-large'];
 
-export default Ember.Component.extend({
+export default UIKindComponent.extend({
   layout,
-  tagName: '',
 
   fontAwesomeSize: Ember.computed('size', function() {
     const size = this.get('size');
