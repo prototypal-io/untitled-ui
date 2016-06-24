e695dd7606eff525173a34bdf17e3313847374a0
Eric Kelly <heroiceric@gmail.com>
2016-06-24 14:27:22 -0400

    Use dynamic prefix for component class

--- addon/helpers/-ui-component-class.js
+++ addon/helpers/-ui-component-class.js
@@ -3,8 +3,30 @@ import Ember from 'ember';
 const FONT_SIZE_PATTERN = /font-size/;
 const FONTAWESOME_PATTERN = /^fa-/;
 
-export default Ember.Helper.helper(function ([prefix, ...classNames]) {
-  let normalizedClassNames = classNames.reduce(function(names, name) {
+const {
+  isPresent
+} = Ember;
+
+export function uiComponentClass(classNames, options = {}) {
+  let { prefix } = options;
+  let normalizedClassNames = normalizeClassNames(classNames);
+
+  return normalizedClassNames.reduce((classes, name) => {
+    switch (true) {
+      case (name === ':component'):
+        return isPresent(prefix) ? `${classes}${prefix} ` : classes;
+      case (isFontSize(name) || isFontAwesome(name)):
+        return `${classes}${name} `;
+      case (isPresent(prefix) && isPresent(name)):
+        return `${classes}${prefix}--${name} `;
+      default:
+        return `${classes}${name} `;
+    }
+  }, '');
+}
+
+function normalizeClassNames(classNames) {
+  return classNames.reduce(function(names, name) {
     if (typeof name === 'string') {
       name.split(/\s/).forEach(function(part) {
         if (part && part !== '') {
@@ -15,18 +37,14 @@ export default Ember.Helper.helper(function ([prefix, ...classNames]) {
 
     return names;
   }, []);
+}
 
-  return normalizedClassNames.reduce(function(string, name) {
-    switch (true) {
-      case (name === ':component'):
-        var baseClass = prefix.replace(/(.*)--$/, '$1');
-        return string += `${baseClass} `;
-      case (FONT_SIZE_PATTERN.test(name)):
-        return string += `${name} `;
-      case (name === 'fa' || FONTAWESOME_PATTERN.test(name)):
-        return string += `${name} `;
-      default:
-        return string += `${prefix}${name} `;
-    }
-  }, '');
-});
+function isFontAwesome(name) {
+  return name === 'fa' || FONTAWESOME_PATTERN.test(name);
+}
+
+function isFontSize(name) {
+  return FONT_SIZE_PATTERN.test(name);
+}
+
+export default Ember.Helper.helper(uiComponentClass);
